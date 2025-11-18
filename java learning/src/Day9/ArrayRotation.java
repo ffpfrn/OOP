26adb324ec152a2a92eb342cbd642c441c580d30
ffpfrn <mohammedfarhanfff>
2025-11-18 22:17:43 +0530

    Add Day9 folder

--- java learning/src/Day9/ArrayRotation.java
+++ java learning/src/Day9/ArrayRotation.java
@@ -1,4 +1,50 @@
 package Day9;
+import java.util.Arrays;
 
-public class ArrayRotation {
-}
+    public class ArrayRotation {
+
+        public static void leftRotateBruteForce(int[] arr, int k) {
+            int n = arr.length;
+            k = k % n;
+            for (int i = 0; i < k; i++) {
+                int temp = arr[0];
+                for (int j = 0; j < n - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[n - 1] = temp;
+            }
+        }
+
+        public static void leftRotateRecursive(int[] arr, int k) {
+            int n = arr.length;
+            k = k % n;
+            if (k > 0) {
+                rotateByOne(arr);
+                leftRotateRecursive(arr, k - 1);
+            }
+        }
+
+        private static void rotateByOne(int[] arr) {
+            int n = arr.length;
+            int temp = arr[0];
+            for (int i = 0; i < n - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[n - 1] = temp;
+        }
+
+        public static void main(String[] args) {
+            int[] arr1 = {1, 2, 3, 4, 5};
+            int k = 2;
+
+            System.out.println("Original Array: " + Arrays.toString(arr1));
+            leftRotateBruteForce(arr1, k);
+            System.out.println("After Brute Force Left Rotation by " + k + ": " + Arrays.toString(arr1));
+
+            int[] arr2 = {1, 2, 3, 4, 5};
+            leftRotateRecursive(arr2, k);
+            System.out.println("After Recursive Left Rotation by " + k + ": " + Arrays.toString(arr2));
+        }
+    }
+
+
--- java learning/src/Day9/RemoveDuplicates.java
+++ java learning/src/Day9/RemoveDuplicates.java
@@ -1,4 +1,26 @@
 package Day9;
 
-public class RemoveDuplicates {
-}
+import java.util.Arrays;
+
+    public class RemoveDuplicates {
+        public static int[] removeDuplicates(int[] arr) {
+            Arrays.sort(arr);
+            int n = arr.length;
+            int j = 0;
+            for (int i = 1; i < n; i++) {
+                if (arr[i] != arr[j]) {
+                    j++;
+                    arr[j] = arr[i];
+                }
+            }
+            return Arrays.copyOf(arr, j + 1);
+        }
+
+        public static void main(String[] args) {
+            int[] arr = {4, 2, 9, 2, 4, 1, 9, 5};
+            int[] result = removeDuplicates(arr);
+            System.out.println(Arrays.toString(result));
+        }
+    }
+
+
