c6d6c40f896ebca9d80b5deac71720c8017b14b0
ffpfrn <mohammedfarhanfff>
2025-11-21 22:12:31 +0530

    feat:Added

---
+++ src/Day5/AB.java
@@ -0,0 +1,28 @@
+package Day5;
+
+abstract class Shape{
+    abstract void perim();
+    public void area(){
+        System.out.println("Area of rectangle");
+    }
+}
+class circle extends Shape{
+    public void perim(){
+        System.out.println("Perimeter of circle");
+    }
+}
+class Rectangle extends Shape{
+    @Override
+    void perim() {
+        System.out.println("Perimeter of rectangle");
+    }
+}
+public class AB {
+    public static void main(String[] args) {
+        Shape rec = new Rectangle();
+        rec.perim();
+        Shape cir = new circle();
+        cir.perim();
+    }
+
+}
